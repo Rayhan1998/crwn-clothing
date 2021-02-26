dda961565fea8c10838b518cfd2f901691c00ccc
Rayhan1998 <miahrayhan01@gmail.com>
2021-02-25 19:27:06 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Homepage from "./Homepagecomponent";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
